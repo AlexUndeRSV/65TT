35045475a245d7d5b04de63e8b18ef41155f705d
AlexUnder Risev <ulsu-super@yandex.ru>
2018-10-31 19:50:28 +0300

    Optimized imports

--- app/src/main/java/com/lynx/testtask65apps/data/db/DBHelper.java
+++ app/src/main/java/com/lynx/testtask65apps/data/db/DBHelper.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.lynx.testtask65apps.other.Constants;
 import com.lynx.testtask65apps.other.Constants.Database;
 
 public class DBHelper extends SQLiteOpenHelper {
--- app/src/main/java/com/lynx/testtask65apps/data/net/WorkersApi.java
+++ app/src/main/java/com/lynx/testtask65apps/data/net/WorkersApi.java
@@ -2,11 +2,7 @@ package com.lynx.testtask65apps.data.net;
 
 import com.lynx.testtask65apps.domain.dataclass.RequestResult;
 
-import java.util.List;
-
-import io.reactivex.Observable;
 import io.reactivex.Single;
-import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface WorkersApi {
